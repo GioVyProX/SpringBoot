4390c31a33c74272bf934fdbd2ae348e004f2684
Giovanni Mauro <giomauro27@gmail.com>
2023-05-21 15:34:59 +0200

    Esercizio 1

--- src/main/java/com/example/springboot/HelloController.java
+++ src/main/java/com/example/springboot/HelloController.java
@@ -1,14 +1,16 @@
 package com.example.springboot;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
 
-	@GetMapping("/")
+	@GetMapping("/hello")
 	public String index() {
-		return "Greetings from Spring Boot!";
+		return "Ciao caro fai 10 diesel";
 	}
+
 
 }
