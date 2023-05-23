4fa84d6745939fd378babfe7b594f822aac719ff
Giovanni Mauro <giomauro27@gmail.com>
2023-05-23 18:10:12 +0200

    Esercizio 2.3

--- src/main/java/com/example/springboot/Ex2.java
+++ src/main/java/com/example/springboot/Ex2.java
@@ -3,6 +3,7 @@ package com.example.springboot;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Arrays;
@@ -13,28 +14,38 @@ import java.util.Objects;
 public class Ex2 {
 
     private List<Meal> meals = Arrays.asList(
-            new Meal("A Meal made by the chef", "19.99","Chef's Special"),
-            new Meal("Chicken burger bacon", "9.99","Chicken burger"),
-            new Meal("Hamburgher with bacon and fries", "10.99","Hamburgher"));
+            new Meal("A Meal made by the chef", "19.99", "Chef's Special"),
+            new Meal("Chicken burger bacon", "9.99", "Chicken burger"),
+            new Meal("Hamburgher with bacon and fries", "10.99", "Hamburgher"));
 
-@GetMapping("/meals")
-    public List<Meal> index (){
+    @GetMapping("/meals")
+    public List<Meal> index() {
         return meals;
     }
 
 
     @GetMapping("/meals/{name}")
-    public Meal mealByName (@PathVariable ("name") String name){
+    public Meal mealByName(@PathVariable("name") String name) {
 
         for (Meal meal : meals) {
             if (Objects.equals(meal.name, name)) {
-
                 return meal;
-
             }
             return null;
         }
+        return null;
+    }
 
+
+    @GetMapping ("/meal")
+    public ResponseEntity<?> getByDescription (
+            @RequestParam ("description") String description){
+
+        for (Meal meal: meals) {
+            if (meal.description.contains(description)){
+                return ResponseEntity.ok(meal);
+            }
+        }
 
         return null;
     }
