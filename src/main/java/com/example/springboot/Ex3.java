8652baedb7522a373761959c4d7b2e39efff242d
Giovanni Mauro <giomauro27@gmail.com>
2023-05-24 17:16:56 +0200

    Esercizio 3.3

--- src/main/java/com/example/springboot/Ex3.java
+++ src/main/java/com/example/springboot/Ex3.java
@@ -3,6 +3,11 @@ package com.example.springboot;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -15,14 +20,14 @@ public class Ex3 {
     public ResponseEntity<List<Meal>> getMeal(){
         return ResponseEntity.ok(meals);
     }
-    @PutMapping(value = "/put/meal")
-    public ResponseEntity<String> putMeal (@RequestBody Meal meal){
+    @PutMapping(value = "/put/meal/ex3")
+    public ResponseEntity <String> putMeal(@RequestBody Meal meal) {
         this.meals.add(meal);
         return ResponseEntity.ok("Meal added!");
     }
 
     @PostMapping (value = "/meal/{name}")
-    public ResponseEntity<String> postMeal (@PathVariable String name, @RequestBody Meal meal){
+    public ResponseEntity<String> postMeal(@PathVariable String name, @RequestBody Meal meal){
 
         this.meals.removeIf(m -> m.name.equals(name));
         this.meals.add(meal);
